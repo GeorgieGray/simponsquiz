1980bed6111c659c1a09c49ed93118b179b41cc6
Georgie Gray <georginamirgray@gmail.com>
2022-08-30 00:40:16 +0100

    Add result page, add reset button

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -110,13 +110,12 @@ const clear = () => {
 let points =0 
 const  addPoint=() => {
     points++
-    console.log(points)
 }
 
-const addQuestion = (question) => {
+const displayQuestion = (question) => {
     const h2 = document.createElement("h2")
-    const newContent = document.createTextNode(question.prompt)
-    h2.appendChild(newContent)
+    const prompt = document.createTextNode(question.prompt)
+    h2.appendChild(prompt)
     mainElement.appendChild(h2)
     const ol = document.createElement('ol')
 
@@ -132,7 +131,6 @@ const addQuestion = (question) => {
             nextQuestion()
         })
         ol.appendChild(li)
-        console.log(question.options[index],  index === question.answer)
     
     }
 
@@ -143,17 +141,71 @@ let activeQuestion = 0
 
 const displayActiveQuestion = () => {
     clear()
-    // addQuestion(questions[activeQuestion])
-    if (activeQuestion > (questions.length - 1)) {
-        alert('GAME OVER ' + points)
+    displayQuestion(questions[activeQuestion])
+}
+
+const getResultQuote = (points) => {
+    if (points < 4) {
+        return 'less than 4'
+    } else if (points < 7) {
+        return 'less than 7 but greater than 4'
     } else {
-        addQuestion(questions[activeQuestion])
+        return 'greater than 7'
     }
 }
 
-const nextQuestion = (event) => {
+
+const displayResult = () => {
+    clear()
+    const container = document.createElement('div')
+    container.classList.add('results')
+    
+    const heading = document.createElement('h1')
+    const headingText = document.createTextNode('GAME OVER')
+    heading.appendChild(headingText)
+
+    // const image = document.createElement('img')
+
+    const quote = document.createElement('p')
+    quote.classList.add('quote')
+    const quoteText = document.createTextNode(getResultQuote(points))
+    quote.appendChild(quoteText)
+
+    const score = document.createElement('p')
+    score.classList.add('score')
+    const scoreText = document.createTextNode(points + '/' + questions.length)
+    score.appendChild(scoreText)
+
+    const button = document.createElement('button')
+    const buttonText = document.createTextNode('Restart')
+    button.appendChild(buttonText)
+    button.addEventListener('click', () => {
+        activeQuestion = 0
+        points= 0
+        displayActiveQuestion()
+        
+    
+    })
+
+    container.appendChild(heading)
+    container.appendChild(quote)
+    container.appendChild(score)
+    container.appendChild(button)
+
+    mainElement.appendChild(container)
+    
+     
+
+}
+
+const nextQuestion = () => {
     activeQuestion++
-    displayActiveQuestion()
+    
+    if (activeQuestion > (questions.length - 1)) {
+        displayResult()
+    } else {
+        displayActiveQuestion()
+    }
 }
 
 const startButton = document.querySelector('#start-game')
